742fac7c15abc7fac7cbad7554bbddded1c3cce0
kevinkkimutai <kiruikevinkim@gmail.com>
2022-12-28 15:47:03 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+
 // code your solution here
+function wonSuperBowl(element) {
+  return element.result === "W"
+}
+
+function superbowlWin(record) {
+  let result = record.find(wonSuperBowl)
+  if (result){
+      return result.year
+  }
+}
